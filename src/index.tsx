849627239e8eed4cd2395a7cb87a1f80445664bf
Reawpai Chunsoi <reawpai.chun@yahoo.com>
2020-10-25 13:22:02 +0700

    Add reducer example and implement redux, redux-trunk

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 import "./index.css";
 import App from "./domain/App";
 import reportWebVitals from "./reportWebVitals";
 
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
 
